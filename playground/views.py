518e554d0a09963c13dd1093f5e4223300e098bb
IanD1013 <ian.tung1013@gmail.com>
2025-02-12 17:28:08 +1300

    Managers and query sets

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Ian'})
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product.title)
+
+    return render(request, 'hello.html', {'name': 'mosh'})
