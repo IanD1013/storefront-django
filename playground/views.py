ad51125834fd21953c80bcc77ddf125776b14a7c
IanD1013 <ian.tung1013@gmail.com>
2025-02-13 09:06:31 +1300

    Retrieve and filter objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    # keyword=value
+    queryset1 = Product.objects.filter(unit_price=20)
+    queryset2 = Product.objects.filter(unit_price__gt=20)
+    queryset3 = Product.objects.filter(unit_price__range=(20, 30))
+    queryset4 = Product.objects.filter(collection__id__range=(1, 2, 3))  # collection_id in (1, 2, 3)
+    queryset5 = Product.objects.filter(title__icontains='coffee')        # title like '%coffee%'
+    queryset6 = Product.objects.filter(title__startswith='coffee')        
+    queryset7 = Product.objects.filter(last_update__year=2021)        
+    queryset8 = Product.objects.filter(description__isnull=True)        
 
-    for product in query_set:
-        print(product.title)
-
-    return render(request, 'hello.html', {'name': 'mosh'})
+    return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset5)})
