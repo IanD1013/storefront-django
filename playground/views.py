2205d3951cec8f3649c8a29288d3403133b52445
IanD1013 <ian.tung1013@gmail.com>
2025-02-14 11:38:41 +1300

    Limit results

--- playground/views.py
+++ playground/views.py
@@ -4,14 +4,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    queryset = Product.objects.order_by('title')
-    queryset = Product.objects.order_by('-title')
-    queryset = Product.objects.order_by('unit_price', '-title')
-    queryset = Product.objects.order_by('unit_price', '-title').reverse()
-    queryset = Product.objects.filter(collection_id=1).order_by('unit_price')
-
-    product = Product.objects.order_by('unit_price')[0] # get the first product not query set
-    product = Product.objects.earliest('unit_price') # earlist returns object, order_by returns query_set
-
+    queryset = Product.objects.all()[:5]
 
     return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset)})
