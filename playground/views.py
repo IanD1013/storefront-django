a3721f2dee540b1e317d658d0711cbc868814afb
IanD1013 <ian.tung1013@gmail.com>
2025-02-14 12:10:30 +1300

    Select fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    queryset = Product.objects.all()[:5]
-
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    
+    
     return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset)})
