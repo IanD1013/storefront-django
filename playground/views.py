380c3aebd58078e2809d57b630d7e982554a8e1e
IanD1013 <ian.tung1013@gmail.com>
2025-02-14 11:19:12 +1300

    Complex lookups using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    # keyword=value
-    queryset1 = Product.objects.filter(unit_price=20)
-    queryset2 = Product.objects.filter(unit_price__gt=20)
-    queryset3 = Product.objects.filter(unit_price__range=(20, 30))
-    queryset4 = Product.objects.filter(collection__id__range=(1, 2, 3))  # collection_id in (1, 2, 3)
-    queryset5 = Product.objects.filter(title__icontains='coffee')        # title like '%coffee%'
-    queryset6 = Product.objects.filter(title__startswith='coffee')        
-    queryset7 = Product.objects.filter(last_update__year=2021)        
-    queryset8 = Product.objects.filter(description__isnull=True)        
+    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20) 
+    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+     
 
-    return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset5)})
+    return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset)})
