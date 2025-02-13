664b1b03f684358d7e822fbbf4fb68f5ecd02fdb
IanD1013 <ian.tung1013@gmail.com>
2025-02-14 11:35:59 +1300

    Sorting

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,14 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
-    # queryset = Product.objects.filter(inventory=F('unit_price')) 
-    queryset = Product.objects.filter(inventory=F('collection__id')) 
+    queryset = Product.objects.order_by('title')
+    queryset = Product.objects.order_by('-title')
+    queryset = Product.objects.order_by('unit_price', '-title')
+    queryset = Product.objects.order_by('unit_price', '-title').reverse()
+    queryset = Product.objects.filter(collection_id=1).order_by('unit_price')
+
+    product = Product.objects.order_by('unit_price')[0] # get the first product not query set
+    product = Product.objects.earliest('unit_price') # earlist returns object, order_by returns query_set
+
 
     return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset)})
