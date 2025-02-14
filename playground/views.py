f93278d98810894c24b65b8b071876d0244b8214
IanD1013 <ian.tung1013@gmail.com>
2025-02-15 11:51:41 +1300

    Select related objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 def say_hello(request):
-    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    # selected_related (1)
+    # prefetch_related (n)
+    # queryset = Product.objects.select_related('collection').all()  
+    # queryset = Product.objects.select_related('collection__someOtherField').all()  
+    # queryset = Product.objects.prefetch_related('promotions').all()  
+    # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()  
     
-    queryset = Product.objects.only('id', 'title') 
-    queryset = Product.objects.defer('description') 
+    # Get the last 5 orders with their customer and items (incl product)
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
     
-    
-    return render(request, 'hello.html', {'name': 'mosh', 'products': list(queryset)})
+    return render(request, 'hello.html', { 'name': 'mosh' })
