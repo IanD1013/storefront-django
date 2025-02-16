ecc679d70ac4b9577095d42d99fbd0ecbfe4f2e6
IanD1013 <ian.tung1013@gmail.com>
2025-02-16 22:08:10 +1300

    Aggregate objects

--- playground/views.py
+++ playground/views.py
@@ -2,16 +2,10 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem, Order
+from django.db.models.aggregates import Count, Sum, Avg, Max, Min
 
 def say_hello(request):
-    # selected_related (1)
-    # prefetch_related (n)
-    # queryset = Product.objects.select_related('collection').all()  
-    # queryset = Product.objects.select_related('collection__someOtherField').all()  
-    # queryset = Product.objects.prefetch_related('promotions').all()  
-    # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()  
+    result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
+    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
     
-    # Get the last 5 orders with their customer and items (incl product)
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    
-    return render(request, 'hello.html', { 'name': 'mosh' })
+    return render(request, 'hello.html', { 'name': 'mosh', 'result': result })
