9354eb01f212bee41a63bd0e748d3eb7718843da
IanD1013 <ian.tung1013@gmail.com>
2025-02-18 09:50:50 +1300

    Group data

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models import Q, F
 from django.db.models.functions import Concat
 from django.core.exceptions import ObjectDoesNotExist
@@ -7,7 +7,8 @@ from store.models import Product, OrderItem, Order, Customer
 from django.db.models.aggregates import Count, Sum, Avg, Max, Min
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')) 
-    queryset = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
+    queryset = Customer.objects.annotate(
+        orders_count=Count('order'),
+    )
     
     return render(request, 'hello.html', { 'name': 'mosh', 'result': list(queryset) })
