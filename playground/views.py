432db8f27de68a6e1ddd0f6c7d6ae252d28a9a26
IanD1013 <ian.tung1013@gmail.com>
2025-02-19 13:41:19 +1300

    Custom manager

--- playground/views.py
+++ playground/views.py
@@ -4,15 +4,6 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-    # first need to fint the content type id for the product model
-    content_type = ContentType.objects.get_for_model(Product)
-
-    # next we can use the content type id to filter the tagged items
-    queryset = TaggedItem.objects \
-        .select_related('tag') \
-        .filter(
-            content_type=content_type, 
-            object_id=1
-        )
+    TaggedItem.objects.get_tags_for(Product, 1)
     
     return render(request, 'hello.html', { 'name': 'mosh', 'tags': list(queryset) })
