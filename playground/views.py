7d42035ac3416fa6f49cea922e9a895fbb567003
IanD1013 <ian.tung1013@gmail.com>
2025-02-19 15:32:31 +1300

    Understand queryset cache

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,8 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-    TaggedItem.objects.get_tags_for(Product, 1)
+    queryset = Product.objects.all()
+    list(queryset)
+    queryset[0]
     
     return render(request, 'hello.html', { 'name': 'mosh', 'tags': list(queryset) })
