e691ac0acbbb2ec4901ca977ea0253cb00f5337a
IanD1013 <ian.tung1013@gmail.com>
2025-02-19 22:51:29 +1300

    Select related objects

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection'] # preload / eager load the collection object
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -14,6 +18,11 @@ class ProductAdmin(admin.ModelAdmin):
         return 'OK'
 
 # admin.site.register(models.Product, ProductAdmin) 
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
