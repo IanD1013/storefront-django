9bad449b187ba12f2b7090d52fbb9384e8032483
IanD1013 <ian.tung1013@gmail.com>
2025-02-19 15:58:58 +1300

    Delete objects

--- playground/views.py
+++ playground/views.py
@@ -4,12 +4,10 @@ from store.models import Product, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
-    collection = Collection.objects.get(pk=11) # get the collection from database first
-    collection.featured_product = None
-    collection.save()
+    collection = Collection(pk=11)
+    collection.delete()
 
-    # another way to do the same thing
-    Collection.objects.filter(pk=11).update(featured_product=None) 
+    Collection.objects.filter(id__gt=5).delete()
   
     
     return render(request, 'hello.html', { 'name': 'mosh' })
