d9549a3a58437de3dd96c541145393ff33d349dd
IanD1013 <ian.tung1013@gmail.com>
2025-02-19 16:18:03 +1300

    Execute raw sql queries

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 # @transaction.atomic()
 def say_hello(request):
+    queryset = Product.objects.raw('SELECT * FROM store_product')
 
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    # sometimes we want to execute queries that don't map to our model objects,
+    # in those cases we can access the database directly, bypass the model layer
+    with connection.cursor() as cursor: 
+        cursor.execute()
+        cursor.callproc('get_customers', [1, 2, 'a'])
+   
     
-    return render(request, 'hello.html', { 'name': 'mosh' })
+    return render(request, 'hello.html', { 'name': 'mosh', 'result': list(queryset) })
