55462b272044bad205d9325d649997688500c3de
IanD1013 <ian.tung1013@gmail.com>
2025-03-03 18:46:47 +1300

    Manage media files

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,4 +1,6 @@
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,4 +14,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls))
-]
+] 
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
